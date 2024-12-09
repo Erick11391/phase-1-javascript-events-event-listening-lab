a43a97f8d56ab8037710393e315720dde72c061a
Erick11391 <xandererik4@gmail.com>
2024-12-09 23:12:56 +0300

    modified

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 function addingEventListener() {
-}
+    
+        const button = document.getElementById('button');
+      
+        if (button) {
+          button.addEventListener('click', function () {
+            alert('Button was clicked!');
+          });
+        }
+      }
+      
+
